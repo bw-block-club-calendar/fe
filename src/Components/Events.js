a4f70e075ff17069f22db4cbcca271d49dcb246f
Robert <Robert.Elias@live.com>
2019-11-18 14:45:43 -0800

    Created event forms and Card.js components

--- src/Components/Events.js
+++ src/Components/Events.js
@@ -13,39 +13,112 @@
 
 import React, { useEffect, useState } from "react";
 
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+    container: {
+      display: 'flex',
+      flexWrap: 'wrap',
+    },
+    textField: {
+      marginLeft: theme.spacing(1),
+      marginRight: theme.spacing(1),
+      width: 200,
+    },
+  }));
+
 const Events =({values, errors, touched, status}) => {
     const [users, setUsers] = useState([]);
-
+    const classes = useStyles();
     useEffect (()=> {status && setUsers (users => [...users, status])}, [status]);
 
     return (
 
         
-   
-    <div className = "event-form">
-    <br></br>
-    <form>
-    <field type="text" name="email" placeholder="What is your email?"/>
-    <field type="text" name="username" placeholder="What is your username?"/>
-    <field type="text" name="password" placeholder="What is your password?"/>
-    <field type="text" name="streetAddress" placeholder="What is your street address?"/>
-    <field type="text" name="city" placeholder="What city?"/>
-    <field type="text" name="zipcode" placeholder="What is your zipcode?"/>
-    <field type="text" name="businessName" placeholder="What is your business name?"/>
-    
-    </form>
-    {users.map( (user, index) => (
-        <ul key={index}>
-        <li>{user.email}</li>
-        <li>{user.username}</li>
-        <li>{user.password}</li>
-        <li>{user.streetAddress}</li>
-        <li>{user.zipcode}</li>
-        <li>{user.businessName}</li>
+        <div className={classes.container}>
+        <div>
+          <TextField
+            id="standard-full-width"
+            label="Label"
+            style={{ margin: 8 }}
+            name="email"
+            placeholder="What is your email?"
+            helperText="Full width!"
+            fullWidth
+            margin="normal"
+            InputLabelProps={{
+              shrink: true,
+            }}
+          />
 
-        </ul>
-    ))}
-    </div>
+          <TextField
+          id="standard-full-width"
+          label="Label"
+          style={{ margin: 8 }}
+          name="streetAddress"
+          placeholder="What is your street address?"
+          helperText="Full width!"
+          fullWidth
+          margin="normal"
+          InputLabelProps={{
+            shrink: true,
+          }}
+        />
+
+        <TextField
+        id="standard-full-width"
+        label="Label"
+        style={{ margin: 8 }}
+        name="city"
+        placeholder="What city?"
+        helperText="Full width!"
+        fullWidth
+        margin="normal"
+        InputLabelProps={{
+          shrink: true,
+        }}
+      />
+
+      <TextField
+      id="standard-full-width"
+      label="Label"
+      style={{ margin: 8 }}
+      name="zipcode"
+      placeholder="What is your zipcode?"
+      helperText="Full width!"
+      fullWidth
+      margin="normal"
+      InputLabelProps={{
+        shrink: true,
+      }}
+    />
+
+    <TextField
+    id="standard-full-width"
+    label="Label"
+    style={{ margin: 8 }}
+    name="businessName"
+    placeholder="What is your business name?"
+    helperText="Full width!"
+    fullWidth
+    margin="normal"
+    InputLabelProps={{
+      shrink: true,
+    }}
+  />
+          </div>
+          {users.map( (user, index) => (
+            <ul key={index}>
+            <li>{user.email}</li>
+            <li>{user.streetAddress}</li>
+            <li>{user.city}</li>
+            <li>{user.zipcode}</li>
+            <li>{user.businessName}</li>
+
+            </ul>
+        ))}
+          </div>
 
     );
 
