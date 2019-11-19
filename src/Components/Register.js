1660b92223b2eabc8359507d89dd261480531d33
Robert <Robert.Elias@live.com>
2019-11-18 20:41:43 -0800

    Validation Schema Yup

--- src/Components/Register.js
+++ src/Components/Register.js
@@ -26,8 +26,8 @@ return(
     </label>
     <br></br>
     <label className="checkbox-container">CommunityMember
-    <Field type="checkbox" name="organization" checked={values.terms}/>
-    {touched.organization && errors.organization && (<p className="errors">{errors.organization}</p>)}
+    <Field type="checkbox" name="CommunityMember" checked={values.terms}/>
+    {touched.CommunityMember && errors.CommunityMember && (<p className="errors">{errors.CommunityMember}</p>)}
     <span className="checkmark"/>
     </label>
     <br></br>
@@ -79,26 +79,29 @@ return(
 
 
 const FormikUserForm = withFormik({
-    mapPropsToValues({email, streetAddress, city, zipcode, businessName, terms}){
+    mapPropsToValues({email, streetAddress, city, zipcode, businessName, terms, organization, CommunityMember}){
         return{
             
             email: email || "",
-            address: streetAddress || "",
+            streetAddress: streetAddress || "",
             city: city || "",
             zipcode: zipcode || "",
             businessName: businessName || "",
+            organization: organization || false,
+            CommunityMember: CommunityMember || false,
             terms: terms || false,
         };
 
     },
     validationSchema: Yup.object().shape({
         
-        email: Yup.string().email("Must be real Email"),
-        address: Yup.string().required("Your address is required"),
+        email: Yup.string().email("Must be real Email").required("email is required"),
+        streetAddress: Yup.string().required("Your address is required"),
         city: Yup.string().min(3,"City too short").max(15,"City too long").required("You city is required"),
         zipcode: Yup.string().min(5, "Your zipcode is too short ").required("Please enter a zipcode!!"),
         businessName: Yup.string().required("Business name is required!!"),
-        terms: Yup.boolean().test('terms', "Please agree to terms of service", value => value === true)        
+        terms: Yup.boolean().test('terms', "Please agree to terms of service", value => value === true),
+        organization: Yup.boolean().test('terms', "Please Check One", value => value === true)        
       }),
       handleSubmit(values, { setStatus, resetForm }) {
         // values is our object with all of our data
